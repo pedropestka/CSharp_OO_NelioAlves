9ac127bad285e6ec0bb22d5a4e7ccb2a7df57bce
Pedro Machado Pestka <pedro.pestka@hotmail.com>
2021-07-12 21:03:48 -0300

    initial commit

---
+++ S04/Aula39_com_OO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula39_com_OO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
