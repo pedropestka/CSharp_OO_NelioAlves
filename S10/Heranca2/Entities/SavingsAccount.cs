e2e97954623def49e34ae8a9fa80234120129e11
Pedro Machado Pestka <pedro.pestka@hotmail.com>
2021-07-13 21:45:59 -0300

    SEÇÃO 10

---
+++ S10/Heranca2/Entities/SavingsAccount.cs
@@ -0,0 +1,28 @@
+﻿namespace Heranca2.Entities
+{
+    sealed class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public override void Withdraw(double amount) // sobreescrevendo o método
+        {
+            base.Withdraw(amount); // reaproveita o método da superclasse e
+            Balance -= 2.0; // acrecenta característica específica da subclasse
+        }
+    }
+}
---
+++ S10/Heranca2/Program.cs
@@ -0,0 +1,57 @@
+﻿using Heranca2.Entities;
+using System;
+
+namespace Heranca2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account(1001, "Alex", 500.0);
+            Account acc2 = new SavingsAccount(1002, "Anna", 500.0, 0.01);
+
+            acc1.Withdraw(10.0);
+            acc2.Withdraw(10.0);
+
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+/*
+ * SEALED
+ * usado para nao permitir que uma classe ou método seja herdado
+ * 
+ * 
+ * CLASSE SELADA
+ * Nota: ainda é possível extender a funcionalidade de uma classe selada usando
+ * "extension methods"
+ * 
+ * Exemplo:
+ * 
+ * namespace Course {
+ * sealed class SavingsAccount {
+ * (...)
+ * }
+ * 
+ * 
+ * 
+ * MÉTODO SELADO
+ * Só pode ser aplicado a métodos sobrepostos
+ * 
+ * Exemplo:
+ * 
+ * public sealed override void Withdraw(double amount)
+ * {
+ * base.Withdraw(amount);
+ * Balance -= 2.0;
+ * }
+ * 
+ *  
+ */
